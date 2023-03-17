25f6189a2288a643829e58247edebfe85baa03e2
prachi <Prachi.PK.Kumari@ctw.bmwgroup.com>
2023-03-17 15:42:58 +0000

    code formated

--- app/src/main/java/com/prachi/sample/myapplication/text/NormalTextActivity.kt
+++ app/src/main/java/com/prachi/sample/myapplication/text/NormalTextActivity.kt
@@ -3,11 +3,13 @@ package com.prachi.sample.myapplication.text
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.material.Text
 import androidx.compose.ui.tooling.preview.Preview
 import com.prachi.sample.myapplication.R
 
@@ -20,21 +22,21 @@ class NormalTextActivity : AppCompatActivity() {
                 modifier = Modifier.fillMaxSize(),
                 verticalArrangement = Arrangement.SpaceEvenly,
                 horizontalAlignment = Alignment.CenterHorizontally,
-                content  = {
+                content = {
                     NormalText(getString(R.string.display_normal_text))
                 }
             )
         }
     }
-    
+
     @Composable
-    fun NormalText(displaytext: String){
+    fun NormalText(displaytext: String) {
         Text(text = displaytext)
     }
-    
+
     @Preview
     @Composable
-    fun NormalTextPreciew(){
+    fun NormalTextPreciew() {
         NormalText(getString(R.string.display_normal_text))
     }
 }
